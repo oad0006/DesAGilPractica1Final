66eb23abc7a5f0f0521b0c9f9d0c9d456a9e0ede
oad0006 <oad0006@alu.ubu.es>
2017-10-22 15:56:19 +0200

    Desarrollo en Rama "develop" - Clase Main Encargada de la ejecución del sistema.

---
+++ DesAGilPractica1Final/src/main/Main.java
@@ -0,0 +1,18 @@
+package main;
+/**
+ * Clase main se encarga de la ejecución principal del sistema.
+ * @author Oscar Eduardo Aguado Diaz
+ *
+ */
+public class Main {
+
+	/**
+	 * Función que se encarga de arrancar el sistema.
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		 System.out.println("Ejecución inicial del sistema");		
+		 
+	}
+
+}
