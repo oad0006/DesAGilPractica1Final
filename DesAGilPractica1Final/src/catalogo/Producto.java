123bd372451e34de6000b094c640390419a62a65
oscarAguado <osceda@hotmail.com>
2017-10-22 16:23:12 +0200

    Desarrollo en Rama "featrure_catalogo" - Implementar clase productis Para registrar productos.

---
+++ DesAGilPractica1Final/src/catalogo/Producto.java
@@ -0,0 +1,36 @@
+package catalogo;
+/**
+ * Clase que se encarga de realizar un registro de producto
+ * @author Oscar Eduardo Aguado Diaz
+ *
+ */
+public class Producto {
+	
+	private String _name;
+	
+	private int _quantity;
+	
+	/**
+	 * Contructor
+	 */
+	public Producto(String name, int quantity) {
+		 _name = name;	
+		 _quantity = quantity;
+	}
+	
+	/**
+	 * Asignación de un producto "Venta"
+	 */
+	public void AssignProduct() {
+		_quantity-=1;
+	}
+	
+	/**
+	 * Representación en cadena
+	 * @return
+	 */
+	public String toString() {
+		return "Producto: "+_name +", Cantidad: " + _quantity ;
+	}
+
+}
